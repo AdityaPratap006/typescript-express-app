ffe0e929c879a088fa8870f5356ed0c627aef0f7
Aditya Pratap <adityapratap7344@gmail.com>
2020-08-11 23:39:16 +0530

    some fixes

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cookieSession from 'cookie-session';
-
 import { router } from './routes/loginRoutes';
+import { AppRouter } from './AppRouter';
+import './controllers/LoginController';
+
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ['lacajaen'] }));
 app.use(router);
+app.use(AppRouter.getInstance());
 
 app.listen(4000, () => {
     console.log(`Listening on PORT: 4000`);
